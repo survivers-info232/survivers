317552591cd6b7970b6b78cea1c787e216517ff5
survivers-info232 <47580337+survivers-info232@users.noreply.github.com>
2019-03-29 08:37:19 +0100

    Commentaire

--- survivers/starting_kit/sample_code_submission/model.py
+++ survivers/starting_kit/sample_code_submission/model.py
@@ -1,6 +1,6 @@
 '''
 Sample predictive model.
-You must supply at least 4 methods: 
+You must supply at least 4 methods:
 - fit: trains the model.
 - predict: uses the model to perform predictions.
 - save: saves the model.
@@ -14,16 +14,16 @@ from sklearn.preprocessing import StandardScaler
 import pickle 
 
 from sklearn.tree import DecisionTreeRegressor                  #
-from sklearn.naive_bayes import GaussianNB                      # Les différentes regression
-from sklearn.linear_model import LinearRegression               # que l'on a utlisé
-from sklearn.neighbors.nearest_centroid import NearestCentroid  #
-from sklearn.ensemble import RandomForestClassifier             # 
+from sklearn.naive_bayes import GaussianNB                      #
+from sklearn.linear_model import LinearRegression               # Les différentes régressions que  
+from sklearn.neighbors.nearest_centroid import NearestCentroid  # nous avons utilisé
+from sklearn.ensemble import RandomForestClassifier             #
 from sklearn.ensemble import GradientBoostingRegressor          #
 
-from sklearn.ensemble import BaggingRegressor                   #
-from sklearn.ensemble import BaggingClassifier                  #
-from sklearn.ensemble import VotingClassifier                   # Pour faire voter les regressions
-from sklearn.pipeline import Pipeline                           #
+from sklearn.ensemble import BaggingRegressor       #
+from sklearn.ensemble import BaggingClassifier      # Pour faire voter 
+from sklearn.ensemble import VotingClassifier       # les regressions
+from sklearn.pipeline import Pipeline               #
 
 class model(BaseEstimator):
     def __init__(self):
@@ -35,41 +35,33 @@ class model(BaseEstimator):
         self.num_feat=1
         self.num_labels=1
         self.is_trained=False
-        # Baseline decision tree : score 0.736285037
-        #self.baseline_clf = DecisionTreeRegressor(max_depth=4)    
-        
-        # GradientBoostingRegressor : score 0.7800
-        self.baseline_clf = GradientBoostingRegressor()   # la meilleure regression parmis celle testées
-        
-        #Naivebayes : score 0.2084484036
+
+# Partie avec les régressions
+
+        ''' Baseline decision tree : 0.736285037 '''
+        #self.baseline_clf = DecisionTreeRegressor(max_depth=4)
+     
+        ''' Naivebayes : score 0.2084484036 '''
         #self.baseline_clf = GaussianNB()
         
-        #linaire regression :  score 0.683989487
+        ''' linaire regression : score 0.683989487 '''
         #self.baseline_clf = LinearRegression()
         
-        #random forest : score 0.4795732662
+        ''' random forest : score 0.4795732662 '''
         #self.baseline_clf = RandomForestClassifier()
         
-        #nearest neighbors : score 0.2249313468
+        ''' nearest neighbors : score 0.2249313468 '''
         #self.baseline_clf = NearestCentroid()
+
+
         
-        #self.baseline_clf = BaggingRegressor(base_estimator=[ 
-         #   ('DecisionTreeRegressor4', DecisionTreeRegressor(max_depth=4)),
-          #          ('LinearRegression'      , LinearRegression()),
-           #         ('RandomForestClassifier', RandomForestClassifier()),
-            #        ('NearestCentroid'       , NearestCentroid()),
-              #      ('GaussianNB', GaussianNB())])
+    # Sélection de la meilleur régression
+        ''' GradientBoostingRegressor : score 0.7800 '''
+        self.baseline_clf = GradientBoostingRegressor()  # notre meilleur régression 
 
+# Fin de partie régression
 
-        #self.baseline_clf = VotingClassifier(estimators=[
-          #           ('DecisionTreeRegressor4', DecisionTreeRegressor(max_depth=4)),
-          #           ('LinearRegression'      , LinearRegression()),
-         #          ('RandomForestClassifier', RandomForestClassifier()),
-          #          ('NearestCentroid'       , NearestCentroid()),
-          #           ('GaussianNB', GaussianNB())],
-            #                           voting='soft')
-
-    def fit(self, X, y, what=0):
+    def fit(self, X, y):
         '''
         This function should train the model parameters.
         Here we do nothing in this example...
@@ -83,7 +75,6 @@ class model(BaseEstimator):
         Use data_converter.convert_to_num() to convert to the category number format.
         For regression, labels are continuous values.
         '''
-        #X, y = self.processor(X,y,what)  
         self.num_train_samples = X.shape[0]
         if X.ndim>1: self.num_feat = X.shape[1]
         print("FIT: dim(X)= [{:d}, {:d}]".format(self.num_train_samples, self.num_feat))
@@ -101,31 +92,30 @@ class model(BaseEstimator):
         # of the 2-d ndarray y
         y = y[:,0]
         # Once we have our regression target, we simply fit our model :
-        self.baseline_clf.fit(X, y, what)
+        self.baseline_clf.fit(X, y)
         self.is_trained=True
         
-    def processor(self,X,Y, what=0):                   # le Préprocessing (séparation des données censurées et non censurées) 
-         ### NEW CONTRIBUTION OF THE GROUP  ###        # les tests sont sur le jupyter notebook
+    def processor(self,X,Y, what):                 # Le preprocessing ( séparation des données censurés et non censurés )
+    ###    NEW CONTRIBUTION OH THE GROUP ###       # Les tests sont sur le jupyter notebook
         '''This function should preprocess the data
         Args :
             X : training data matrix
             Y : Training label matrix
             what : if 0 return uncensored data, if 1 return censored data
         '''
-        if (what==0) :                                          # Les données non censuré
+        if (what==0) :                                              # Les données non censuré
             X_uncensored = X[np.where(X[:,X.shape[1]-1]==1)]
             if Y.shape[0]==0 :
-                return (X_uncensored,Y)                         
+                return (X_uncensored)
             else :
                 Y_uncensored = Y[np.where(X[:,X.shape[1]-1]==1)]
-                return (X_uncensored, Y_uncensored)
-            
-        if (what==1) :                                          # Les données censuré
+                return (X_uncensored,Y_uncensored)
+        if (what==1) :                                              # Les données censuré
             X_censored = X[np.where(X[:,X.shape[1]-1]==0)]
             if Y.shape[0]==0 :
-                return (X_censored,Y)                           
+                return (X_censored)
             else :
-                Y_censored = Y[np.where(X[:,X.shape[1]-1]==0)]
+                Y_censored = Y[np.where(X[:,X.shape[1]-1]==1)]
                 return (X_censored,Y_censored)
         
 
